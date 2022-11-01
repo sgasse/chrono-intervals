ecf3c3cea083b3a61b69a87d9442388bc6caae9a
Simon Gasse <sgasse@users.noreply.github.com>
2022-11-01 09:38:51 +0100

    Add zero-sized tests

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -136,6 +136,42 @@ fn test_get_utc_intervals_extended_begin_end() -> Result<(), Error> {
 }
 
 #[test]
+fn test_get_utc_intervals_zero_sized() -> Result<(), Error> {
+    // `end` before `begin`
+    let begin = DateTime::parse_from_rfc3339("2022-11-29T08:23:45.000000Z")?;
+    let end = DateTime::parse_from_rfc3339("2022-10-01T08:23:45.000000Z")?;
+    assert_eq!(
+        get_extended_utc_intervals_with_defaults(begin, end, &Grouping::PerDay, 0),
+        vec![]
+    );
+
+    // `begin` and `end` equal
+    let begin_end = DateTime::parse_from_rfc3339("2022-11-29T08:23:45.000000Z")?;
+    assert_eq!(
+        get_extended_utc_intervals_with_defaults(begin_end, begin_end, &Grouping::PerDay, 0),
+        vec![]
+    );
+
+    // PerDay without extension on sub-day range
+    let begin = DateTime::parse_from_rfc3339("2022-10-29T08:23:45.000000Z")?;
+    let end = DateTime::parse_from_rfc3339("2022-10-29T10:23:45.000000Z")?;
+    assert_eq!(
+        get_utc_intervals_opts(
+            begin,
+            end,
+            &Grouping::PerDay,
+            0,
+            Duration::milliseconds(1),
+            false,
+            false
+        ),
+        vec![]
+    );
+
+    Ok(())
+}
+
+#[test]
 fn test_get_extended_utc_intervals_with_defaults() -> Result<(), Error> {
     // Regular case
     let begin = DateTime::parse_from_rfc3339("2022-10-29T08:23:45.000000Z")?;
