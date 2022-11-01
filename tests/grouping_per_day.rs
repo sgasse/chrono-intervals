0cce005f598d8903bcd4183cb062b49df059499d
Simon Gasse <sgasse@users.noreply.github.com>
2022-11-01 10:39:32 +0100

    Add per-day tests

---
+++ tests/grouping_per_day.rs
@@ -0,0 +1,68 @@
+use chrono::{DateTime, Datelike, NaiveTime, TimeZone, Utc};
+use chrono_intervals::{get_extended_utc_intervals_with_defaults, grouping::Grouping, Error};
+
+#[test]
+fn test_per_day_regular() -> Result<(), Error> {
+    let begin = DateTime::parse_from_rfc3339("2022-06-25T08:23:45.000000Z")?;
+    let end = DateTime::parse_from_rfc3339("2022-06-27T08:23:45.000000Z")?;
+
+    let daily_intervals =
+        get_extended_utc_intervals_with_defaults(begin, end, &Grouping::PerDay, 0);
+    let expected_intervals = vec![
+        (
+            Utc.ymd(2022, 6, 25).and_hms(0, 0, 0),
+            Utc.ymd(2022, 6, 25).and_hms_milli(23, 59, 59, 999),
+        ),
+        (
+            Utc.ymd(2022, 6, 26).and_hms(0, 0, 0),
+            Utc.ymd(2022, 6, 26).and_hms_milli(23, 59, 59, 999),
+        ),
+        (
+            Utc.ymd(2022, 6, 27).and_hms(0, 0, 0),
+            Utc.ymd(2022, 6, 27).and_hms_milli(23, 59, 59, 999),
+        ),
+    ];
+    assert_eq!(daily_intervals, expected_intervals);
+
+    Ok(())
+}
+
+#[test]
+fn test_per_day_over_a_month() -> Result<(), Error> {
+    let begin = DateTime::parse_from_rfc3339("2022-06-25T08:23:45.000000Z")?;
+    let end = DateTime::parse_from_rfc3339("2022-07-25T08:23:45.000000Z")?;
+
+    let daily_intervals =
+        get_extended_utc_intervals_with_defaults(begin, end, &Grouping::PerDay, 0);
+    assert_eq!(daily_intervals.len(), 31);
+    for interval in daily_intervals {
+        assert_eq!(interval.0.day(), interval.1.day());
+        assert_eq!(interval.0.time(), NaiveTime::from_hms(0, 0, 0));
+        assert_eq!(
+            interval.1.time(),
+            NaiveTime::from_hms_milli(23, 59, 59, 999)
+        );
+    }
+
+    Ok(())
+}
+
+#[test]
+fn test_per_day_over_a_year() -> Result<(), Error> {
+    let begin = DateTime::parse_from_rfc3339("2021-06-25T08:23:45.000000Z")?;
+    let end = DateTime::parse_from_rfc3339("2022-06-24T08:23:45.000000Z")?;
+
+    let daily_intervals =
+        get_extended_utc_intervals_with_defaults(begin, end, &Grouping::PerDay, 0);
+    assert_eq!(daily_intervals.len(), 365);
+    for interval in daily_intervals {
+        assert_eq!(interval.0.day(), interval.1.day());
+        assert_eq!(interval.0.time(), NaiveTime::from_hms(0, 0, 0));
+        assert_eq!(
+            interval.1.time(),
+            NaiveTime::from_hms_milli(23, 59, 59, 999)
+        );
+    }
+
+    Ok(())
+}
